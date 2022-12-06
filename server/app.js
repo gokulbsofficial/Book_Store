039a76109a97aadd24a6de5b6448b07ac9d5fb3d
GokulSreejith <gokulsreejith22002@gmail.com>
2022-12-07 00:07:47 +0530

    cors added

--- server/app.js
+++ server/app.js
@@ -1,16 +1,30 @@
-const EXPRESS = require('express');
+const EXPRESS = require("express");
+const cors = require("cors");
 
-const CONFIG = require('./config/config');
+const CONFIG = require("./config/config");
 
-let env = 'development';
 
-const { ENV_STATE, SERVER_ENV,SERVER_PORT } = CONFIG.SERVER;
+const { ENV_STATE, SERVER_ENV, SERVER_PORT } = CONFIG.SERVER;
+
+let env = SERVER_ENV;
 
 const APP = EXPRESS();
+APP.use(
+  cors({
+    origin: [
+      "https://www.book-store.gokulsreejith.com",
+      "https://www.book-store-client.onrender.com",
+      "https://book-store.gokulsreejith.com",
+      "https://book-store-client.onrender.com",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PATCH", "POST", "DELETE", "OPTIONS"],
+  })
+);
 
-require('./config/database.config')(CONFIG[env]);
-require('./config/express')(APP);
-require('./config/routes')(APP);
+require("./config/database.config")(CONFIG[env]);
+require("./config/express")(APP);
+require("./config/routes")(APP);
 
 // Client Routes
 APP.use("/", EXPRESS.static(__dirname + "/dist"));
@@ -18,4 +32,8 @@ APP.use("/", EXPRESS.static(__dirname + "/dist"));
 APP.use("*", EXPRESS.static(__dirname + "/dist"));
 
 APP.listen(SERVER_PORT);
-console.log(`Server is listening on port ${SERVER_PORT} in ${SERVER_ENV} mode & ENV_STATE is ${ENV_STATE ? "ACTIVE" : "INACTIVE"}`);
+console.log(
+  `Server is listening on port ${SERVER_PORT} in ${SERVER_ENV} mode & ENV_STATE is ${
+    ENV_STATE ? "ACTIVE" : "INACTIVE"
+  }`
+);
